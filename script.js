5b75a9c9ca7be3e1658f4583e48707e0046a151d
Chinmay Pandhare <pandharechinmay882@gmail.com>
2016-08-29 17:39:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-function navbar(){
+function resize(){
 	if($(window).width() <= 767) {
 		$('ul').slideUp('fast');
 		$('span.glyphicon').stop().fadeIn('fast');
+		$('.jumbotron').css({'background': "#fff url('Jumbotron-small.jpg') center center no-repeat", "background-size": "auto 100%"});
 	}
 	else {
 		$('ul').slideDown('fast');
 		$('span.glyphicon').stop().fadeOut('fast');
+		$('.jumbotron').css({'background': "#fff url('Jumbotron.jpg') center center no-repeat", "background-size": "95% auto"});
 	}
 }
 
-$(document).load(navbar());
+$(document).load(resize());
 
 $(window).resize(function(){
-	navbar()
+	resize()
 });
 
 $('.close').click(function(){
