da2ef7b1df6cffecc32126d33e2ba291b16abbb9
Chinmay Pandhare <pandharechinmay882@gmail.com>
2016-08-31 15:51:03 +0530

    Create bottom.php

---
+++ bottom.php
@@ -0,0 +1,9 @@
+		<FOOTER ID="footer" CLASS="text-center">
+			&copy; 2016 CENTER FOR ENTREPRENEURIAL LEADERSHIP, BITS PILANI  <BR/>  DESIGN BY CHINMAY PANDHARE.
+		</FOOTER>
+		<!--Scripts-->
+		<SCRIPT SRC="http://code.jquery.com/jquery-latest.min.js"></SCRIPT>
+		<SCRIPT SRC="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></SCRIPT>
+		<SCRIPT SRC="script.js"></SCRIPT>
+	</BODY>
+</HTML>	
