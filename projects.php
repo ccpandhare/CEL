3cfd12ff79a67b9374dc9d3961b2ef1e0ddcbcbd
Chinmay Pandhare <pandharechinmay882@gmail.com>
2016-08-31 15:51:44 +0530

    Create projects.php

---
+++ projects.php
@@ -0,0 +1,35 @@
+<? $projects = 1; $defined="Projects | "; require 'top.php'; ?>
+
+		<DIV CLASS="container text-justify">
+			<DIV CLASS="row">
+				<DIV CLASS="col-md-4">
+					<H3><A HREF="http://conquest.org.in/">Conquest</A></H3>
+					<P>Conquest is an international startup conclave organised by the Center for Entrepreneurial Leadership (CEL) of BITS Pilani. It provides an exemplary platform for emerging startups to step to the fore and grow. Conquest does not cater to veterans! We seek garden fresh ideas that not only change the way we live but also the way we think. To reinforce the spirit of entrepreneurship across the world, we are back with a stronger edition, to take a step forward in realising our vision of revolutionising the startup ecosystem.</P>
+				</DIV>
+				<DIV CLASS="col-md-4">
+					<H3><A HREF="print.php">CEL Print</A></H3>
+					<P>Print is one of the most unique projects run by CEL. Through this project, CEL reaches out to all its stakeholders and fulfills one of its most important objectives- to spread the spirit of entrepreneurship to the student community and beyond. In its new avatar, Print does this by bringing out two publications, Aspire and Imagine. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi congue hendrerit ex commodo fringilla. <A HREF="print.php">....Read More <SPAN CLASS="glyphicon glyphicon-chevron-right"></SPAN></A></P>
+				</DIV>
+				<DIV CLASS="col-md-4">
+					<H3><A HREF="rural.php">RED</A></H3>
+					<P>Rural Entrepreneurship Division (RED) is CEL's rural entrepreneurship project, running in Rajasthan and Goa, and looks to turn our rural location into an advantage, by looking to assist and setup rural enterprises and in the process give a sustainable source of income to village folk in surrounding areas. RED seeks to empower rural areas with entrepreneurship tools such that they themselves can create and manage self sustaining ventures. <A HREF="rural.php">....Read More <SPAN CLASS="glyphicon glyphicon-chevron-right"></SPAN></A></P>
+				</DIV>
+			</DIV>
+
+			<DIV CLASS="row">
+				<DIV CLASS="col-md-4">
+					<H3>SocialWeYou</H3>
+					<P>The specialized cell was created with focus on how businesses are moving from 'Interruption-based' marketing to 'relationship-based' conversations by connecting to people who in-turn become evangelists of a brand. It is this stimulus which changes mere brands to movements led by people. It was the first of its kind in any entrepreneurship cell in India. While the usage of web tools remains indispensible, it will focus on specialised verticals like Non-profits, Startups, e-Governance and e-commerce in the future. SocialWeYou will be the brand that the social media cell of CEL will start up with.</P>
+				</DIV>
+				<DIV CLASS="col-md-4">
+					<H3>Summer in a Startup (SIS)</H3>
+					<P>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi congue hendrerit ex commodo fringilla. Aliquam erat volutpat. Quisque dignissim, odio nec lobortis consectetur, diam arcu dapibus arcu, nec scelerisque massa ligula sed lorem. Etiam dictum vehicula augue non lacinia. Integer at sapien vitae dui dapibus rutrum. Nunc mattis nibh ac.</P>
+				</DIV>
+				<DIV CLASS="col-md-4">
+					<H3>Student Consultancy</H3>
+					<P>The Student Consultancy project at CEL is intended at harnessing the massive untapped talent pool available amongst campus students, into incentivised work. Aiming at providing end-to-end solutions to SMEs, ViMarsh channelizes freelancers in the campus to an interface while simultaneously exposing them to the rigors of matching the industry standards. Amongst its diversified verticals are included IT Solutions, Environment and Ecosystem Management, Organizational Management, H.R., Marketing, Analytics and Optimization, et al.</P>
+				</DIV>
+			</DIV>
+		</DIV>
+		
+<? require 'bottom.php'; ?>
