b2389e231f962d9856c9c53fcc06d0d1313a3c32
Chinmay Pandhare <pandharechinmay882@gmail.com>
2016-08-31 15:50:31 +0530

    Create about.php

---
+++ about.php
@@ -0,0 +1,22 @@
+<? $about = 1; $defined="About | "; require 'top.php'; ?>
+
+		<DIV CLASS="container text-justify">
+			<H1>About Us</H1>
+			<P>The Center for Entrepreneurial Leadership (CEL) is the entrepreneurial community of BITS Pilani which is aimed at creating leaders through entrepreneurial thinking. CEL is recognized as a center for excellence at BITS and works towards creating the right ecosystem for ideas to mature into successful ventures. To fulfill its mandate, CEL has taken up many projects and initiatives to foster the spirit of entrepreneurship on campus. Due to its consistent efforts in this regard, CEL is ranked amongst the top entrepreneurship cells of the country.</P>
+			<H1>Vision and Mission</H1>
+			<P>Our vision is to inspire BITSians to play a prominent role in leading diverse entrepreneurial activities in the country and make a significant contribution in global entrepreneurial innovation.</P>
+			<P>Our mission is to promote entrepreneurial thinking among the BITSian community and create business leaders in all spheres of life.</P>
+			<H1>Goals</H1>
+			<UL>
+					<LI>Sow the seeds of entrepreneurial leadership across all disciplines</LI>
+					<LI>Facilitate entrepreneurial activity on campus</LI>
+					<LI>Bridge the gap between industry and the academic curriculum</LI>
+					<LI>Build a high-quality ecosystem on campus to support resource-strapped startup ventures</LI>
+					<LI>Facilitate commercialization of R&D efforts at BITS</LI>
+					<LI>Inculcate the value of social responsibility in every BITSian</LI>
+			</UL>
+			<H1>Background</H1>
+			<P>The idea for CEL was seeded by the BITSAA Center for Entrepreneurship and Venture Capital (CEVC), with the vision of promoting the spirit of entrepreneurial thinking among the BITSian community . The students and the Institute shared this vision and came together in record time to set up the center. The concerted efforts of the students,alumni and faculty resulted in the Centre (set up within a short span of 6 months) being named as one of the top centres of entrepreneurship among Indian Universities that year.</P>
+		</DIV>
+		
+<? require 'bottom.php'; ?>
