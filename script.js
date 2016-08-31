60c3fd26f7b6c795735af01373a189d4574d1390
Chinmay Pandhare <pandharechinmay882@gmail.com>
2016-08-31 15:52:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function resize(){
 	if($(window).width() <= 767) {
-		$('ul').slideUp('fast');
-		$('span.glyphicon').stop().fadeIn('fast');
+		$('ul.nav').slideUp('fast');
+		$('.close span.glyphicon').stop().fadeIn('fast');
 		$('.jumbotron').css({'background': "#fff url('Jumbotron-small.jpg') center center no-repeat", "background-size": "auto 100%"});
 	}
 	else {
-		$('ul').slideDown('fast');
-		$('span.glyphicon').stop().fadeOut('fast');
+		$('ul.nav').slideDown('fast');
+		$('.close span.glyphicon').stop().fadeOut('fast');
 		$('.jumbotron').css({'background': "#fff url('Jumbotron.jpg') center center no-repeat", "background-size": "95% auto"});
 	}
 }
@@ -14,9 +14,9 @@ function resize(){
 $(document).load(resize());
 
 $(window).resize(function(){
-	resize()
+	resize();
 });
 
 $('.close').click(function(){
-	$('ul').slideToggle('fast');
+	$('ul.nav').slideToggle('fast');
 });
